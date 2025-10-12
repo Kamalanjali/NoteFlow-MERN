6fd27129a0df6f854bd206d785701a7b437883a6
Lakshmi Kamalanjali Mandalika <kamalanjalimetta31@gmail.com>
2025-10-12 20:30:00 +0530

    project commit

---
+++ Backend/src/controllers/notesController.js
@@ -0,0 +1,67 @@
+import Note from "../models/note.js";
+
+export async function getAllNotes(req, res) {
+  try {
+    const notes = await Note.find().sort({ createdAt: -1 }); //newest first
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error("Error in getAllNotes Controller");
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+export async function getNoteById(req, res) {
+  try {
+    const noteById = await Note.findById(req.params.id);
+    if (!noteById) return res.status(404).json({ message: "Note not found" });
+    res.status(200).json(noteById);
+  } catch (error) {
+    console.error("Error in getNoteById Controller");
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+export async function createNote(req, res) {
+  try {
+    const { title, content } = req.body;
+    const note = new Note({ title, content });
+
+    const savednote = await note.save();
+    res.status(201).json(savednote);
+  } catch (error) {
+    console.error("Error in createNote Controller");
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function updateNote(req, res) {
+  try {
+    const { title, content } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      {
+        title,
+        content,
+      },
+      {
+        new: true,
+      }
+    );
+    if (!updatedNote)
+      return res.status(404).json({ message: "Note not found" });
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    console.error("Error in updateNote Controller");
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function deleteNote(req, res) {
+  try {
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deletedNote)
+      return res.status(404).json({ message: "Note not found" });
+    res.status(200).json(deletedNote);
+  } catch (error) {
+    console.error("Error in deleteNote Controller");
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
