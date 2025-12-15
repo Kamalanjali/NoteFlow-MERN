441562d920c1574d1d3cd5c78a40b9d869d334a0
Lakshmi Kamalanjali Mandalika <kamalanjalimetta31@gmail.com>
2025-12-15 23:48:04 +0530

    upgraded the Redis url and updated the rate limiter

--- Backend/src/config/upstash.js
+++ Backend/src/config/upstash.js
@@ -4,7 +4,7 @@ import { Redis } from "@upstash/redis";
 
 dotenv.config();
 
-//create a rate limiter that allows 10 requests per 20 seconds
+//create a rate limiter that allows 100 requests per 60 seconds
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(100, "60 s"),
